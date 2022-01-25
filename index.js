10b74e310f89caf68bf5318442b0ed11cece1f11
Ryan Wibawa <ryan.wibawa@gmail.com>
2022-01-25 09:36:36 -0800

    inline callback method

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const mytools = require('./build/Debug/mytools.node');
 
-const cb = ( error, result ) => {
+mytools.writeFile('./test01.txt', 'lorem ipsum dolor sit amet', ( error, result ) => {
   if (error) {
     console.error( error.message || error );
   } else {
     console.debug(`Result: ${JSON.stringify(result)}`);
   }
-};
-
-mytools.writeFile('./test01.txt', 'lorem ipsum dolor sit amet', cb);
+});
 
 module.exports = mytools;
